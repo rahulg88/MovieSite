c0ff2d3059e3bf752bb450b0927c5337fcc9cc50
rahulg88 <rahulg888756@gmail.com>
2024-09-09 23:47:27 +0530

    changes

--- src/MovieWebsite/Home.jsx
+++ src/MovieWebsite/Home.jsx
@@ -5,6 +5,7 @@ import { Card, Grid } from "@mui/material";
 const Home = () => {
   return (
     <div>
+      <h2>I have just made some changes to this application</h2>
       <Grid container spacing={2} sx={{ width: "100%" }}>
         <Grid item xs={12} sm={6} md={12} lg={12}>
           <Movies />{" "}
